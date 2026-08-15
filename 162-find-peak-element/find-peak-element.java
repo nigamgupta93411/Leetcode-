91b059f94de9039b30356630a1e74796df7a1ba3
nigamgupta93411 <sonunigam.gupta_cs25@gla.ac.in>
2026-08-15 15:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (75.21%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l=0;
+        int r=nums.length-1;
+        int peak=Integer.MIN_VALUE;
+        while(l<r){
+            int mid=(l+r)/2;
+             if(nums[mid]<nums[mid+1]){
+                l=mid+1;
+             }
+             else{
+                r=mid;
+             }
+        }
+        return l;
+        
+    }
+}
