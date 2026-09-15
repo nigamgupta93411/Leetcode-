b5996b175e543abf2b904cd8a3f31230a1f0459e
nigamgupta93411 <sonunigam.gupta_cs25@gla.ac.in>
2026-09-16 01:41:21 +0530

    Time: 20 ms (81.75%) | Memory: 56.3 MB (76.33%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int count=0;
+       Arrays.sort(people);
+       int left=0;
+       int right=people.length-1;
+       while(left<=right){
+        if(people[left]+people[right]<=limit){
+            count++;
+            left++;
+            right--;
+        }
+        else{
+            count++;
+            right--;
+        }
+       }
+       return count;
+    }
+}
