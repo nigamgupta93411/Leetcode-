5f584f76ae20d4f4f8a7db0513dc79348ed1106a
nigamgupta93411 <sonunigam.gupta_cs25@gla.ac.in>
2026-09-16 15:43:07 +0530

    Time: 96 ms (8.41%) | Memory: 48 MB (33.06%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -2,21 +2,58 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character,Integer> map=new HashMap<>();
         int max=0;
-        char[] arr=s.toCharArray();
         int l=0;
-        int h=0;
-
-        for( ;h<arr.length;h++){
-            char ch=arr[h];
-            if(map.containsKey(ch)){
-              l = Math.max(l, map.get(ch) + 1);
-            }
-                map.put(ch,h);
-                max=Math.max(max,h-l+1);
-
-            
+        for(int r=0;r<s.length();r++){
+            char ch=s.charAt(r);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+         while(map.get(ch)>1){
+            char cc=s.charAt(l);
+            map.put(cc,map.getOrDefault(cc,0)-1);
+         if(map.get(cc)==0)
+            map.remove(cc);
+        
+         
+         l++;
+         }
+         max=Math.max(max,r-l+1);
         }
         return max;
+
+         }
+        }
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // int max=0;
+        // char[] arr=s.toCharArray();
+        // int l=0;
+        // int h=0;
+
+        // for( ;h<arr.length;h++){
+        //     char ch=arr[h];
+        //     if(map.containsKey(ch)){
+        //       l = Math.max(l, map.get(ch) + 1);
+        //     }
+        //         map.put(ch,h);
+        //         max=Math.max(max,h-l+1);
+
+            
+        // }
+        // return max;
         
-    }
-}
+//     }
+// }
