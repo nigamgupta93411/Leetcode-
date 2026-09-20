2868a313a4aa304c55384986344e1b6b8e244f1b
nigamgupta93411 <sonunigam.gupta_cs25@gla.ac.in>
2026-09-21 00:25:31 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (95.67%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum=0;
+    for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        int ind=('z'-ch)+1;
+        sum+=ind*(i+1);
+
+    }
+    return sum;
+        
+    }
+}
